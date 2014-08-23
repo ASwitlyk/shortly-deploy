6773e97aa204f4d8a52166c735ef24a6929a3275
Hack Reactor Students <accounts@hackreactor.com>
2014-08-23 10:57:26 -0700

    none

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,7 +65,9 @@ module.exports = function(grunt) {
     concat: {
         dist: {
           src: ['public/lib/handlebars.js','public/lib/underscore.js', 'public/lib/jquery.js',
-                'public/lib/backbone.js', 'public/client/**/*.js'],
+                'public/lib/backbone.js', 'public/client/app.js', 'public/client/link.js',
+                'public/client/links.js', 'public/client/linkView.js', 'public/client/linksView.js',
+                'public/client/createLinkView.js', 'public/client/router.js'],
           dest: 'public/allFiles.js',
         }
     },
