f6074b021960feb65f4b6a11a2da82b3f34f0567
Hack Reactor Students <accounts@hackreactor.com>
2014-08-23 10:47:30 -0700

    change concat order in files

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,8 +31,8 @@ module.exports = function(grunt) {
         force: 'true',
         jshintrc: '.jshintrc',
         ignores: [
-          'public/lib/**/*.js',
-          'public/dist/**/*.js'
+          'public/lib/*.js',
+          'public/dist/*.js'
         ]
       }
     },
@@ -64,7 +64,8 @@ module.exports = function(grunt) {
 
     concat: {
         dist: {
-          src: ['public/lib/**/*.js', 'public/client/**/*.js'],
+          src: ['public/lib/underscore.js', 'public/lib/jquery.js',
+                'public/lib/handlebars.js', 'public/lib/backbone.js', 'public/client/**/*.js'],
           dest: 'public/allFiles.js',
         }
     },
